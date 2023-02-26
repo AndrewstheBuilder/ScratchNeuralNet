49ccab8716d3e6b09d7d0c7297914ab6ca1cc890
Andrews Peter <51387040+AndrewstheBuilder@users.noreply.github.com>
2023-02-25 20:48:47 -0500

    Finished makemore exercises and did meta exercise - increased data set from 32k to 2 million

---
+++ makemore/AppendToNames.ps1
@@ -0,0 +1,21 @@
+﻿# 2052781 records!!
+#Get-Content -Path C:\Users\andre\Downloads\names\*.txt
+#Get-Content -Path C:\Users\andre\Downloads\names\*.txt | Select-String ',' | ForEach-Object { $_.Line.Split(',')[0].ToLower() }
+#Get-Content -Path C:\Users\andre\Downloads\names\*.txt | ForEach-Object { $_.Split(',')[0].ToLower() }
+#Get-Content -Path C:\Users\andre\Downloads\names\*.txt | ForEach-Object { $_.Split(',')[0].ToLower() } | Add-Content -Path C:\Users\andre\Documents\names.txt
+$count = 0
+$total = (Get-ChildItem -Path C:\Users\andre\Downloads\names\*.txt).Count
+Get-ChildItem -Path C:\Users\andre\Downloads\names\*.txt | ForEach-Object {
+    $count++
+    $percent = [Math]::Min(($count / $total) * 100, 100)
+    Write-Progress -Activity "Appending to file" -PercentComplete $percent -Status "File $count of $total"
+    Get-Content -Path $_.FullName | ForEach-Object {
+        $_.Split(',')[0].ToLower()
+    }
+} | Add-Content -Path C:\Users\andre\ScratchNeuralNet\makemore\names-copy.txt
+
+
+
+
+
+
---
+++ makemore/ShuffleNames.ps1
@@ -0,0 +1,7 @@
+﻿$total = (Get-Content -Path C:\Users\andre\ScratchNeuralNet\makemore\names.txt).Count
+$count = 0
+Get-Content -Path C:\Users\andre\ScratchNeuralNet\makemore\names.txt | Sort-Object {Get-Random} | ForEach-Object {
+    $count++
+    Write-Progress -Activity "Shuffling names" -PercentComplete (($count / $total) * 100) -Status "Line $count of $total"
+    $_
+} | Set-Content -Path C:\Users\andre\ScratchNeuralNet\makemore\names_shuffled.txt
